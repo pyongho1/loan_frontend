dd8f55cd55f74c93cb01509452f41e4bc4fe4cd6
Yong Park <pyongho0306@gmail.com>
2024-05-15 18:08:48 -0700

    Edited App.js to add routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AdminView from "./pages/AdminView";
+import NonprofitView from "./pages/NonprofitView";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/admin" component={AdminView} />
+      <Route
+        path="/user/:userId"
+        render={({ match }) => <NonprofitView userId={match.params.userId} />}
+      />
+    </Routes>
+  </Router>
+);
 
 export default App;
